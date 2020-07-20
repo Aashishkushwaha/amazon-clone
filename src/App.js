a82e44256400ac20582fe4f91e2782f6c424478d
Aashish Kushwaha <aashishk1997@gmail.com>
2020-07-21 01:41:46 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+            <h1>Home Page</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
